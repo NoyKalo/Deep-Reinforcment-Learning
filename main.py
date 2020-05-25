7c86e6e4ddde80cfb4659121b546208b41b38724
schlamic <michalschlachet@gmail.com>
2020-05-25 17:08:36 +0300

    script

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from World import World
 import numpy as np
 if __name__ == "__main__":
-
      world = World()
      world.plot()
      world.plot_value([np.random.random() for i in range(world.nStates)])
      world.plot_policy(np.random.randint(1, world.nActions,(world.nStates, 1)))
-
